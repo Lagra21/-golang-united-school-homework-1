8b72f0e71f28078a45b3d68e2e732b7df76b15d0
Olha <gardash2105@gmail.com>
2022-02-20 20:37:36 +0200

    main test was added

--- main_test.go
+++ main_test.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/kyokomi/emoji/v2"
 )
 
-func TestShowWorld(t *testing.T) {
+func TestGetWorld(t *testing.T) {
 	tstStr := GetWorldTxt()
 	refStr := emoji.Sprint(":map:")
 	if tstStr != refStr {
 		t.Errorf("%s it's not a correct pic ", tstStr)
 	}
+}
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
 }
