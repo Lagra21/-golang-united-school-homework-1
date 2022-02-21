40496d679d5bb6abf9d5af272cea80b4c666117f
Olha <gardash2105@gmail.com>
2022-02-21 08:50:06 +0200

    timeout test was added

--- main_test.go
+++ main_test.go
@@ -3,16 +3,21 @@ package main
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/kyokomi/emoji/v2"
 )
 
 func TestGetWorld(t *testing.T) {
 	tstStr := GetWorldTxt()
-	refStr := emoji.Sprint(":map:")
+	refStr := emoji.Sprint(":worldmap:")
 	if tstStr != refStr {
-		t.Errorf("%s it's not a correct pic ", tstStr)
+		t.Errorf("%s it's not a worldmap pic ", tstStr)
 	}
+}
+
+func TestWillTimeout(t *testing.T) {
+	time.Sleep(1 * time.Second)
 }
 
 func TestMain(m *testing.M) {
